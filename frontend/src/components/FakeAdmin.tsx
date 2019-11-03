65b4d3ab68964800a0439a46205d2ed58f252fdb
Pierre <pierre.wan-fat@mines-paristech.fr>
2019-11-03 14:33:11 +0800

    Add FakeAdmin.

---
+++ frontend/src/components/FakeAdmin.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import MainContainer from "./MainContainer";
+import Button from "react-bootstrap/Button";
+
+export default function FakeAdmin() {
+    /**
+     * This is a troll.
+     */
+
+    const [buttonPushed, setButtonPushed] = useState<boolean>(false);
+
+    return (
+        <MainContainer heading="Administration">
+            <Button variant="outline-danger"
+                    onClick={() => setButtonPushed(true)}>
+                Passer en maintenance
+            </Button>
+
+            {
+                buttonPushed &&
+                    <div className="troll">
+                        <span role="img" aria-label="lol">😂😂😂</span>
+                    </div>
+            }
+    </MainContainer>
+    )
+}
