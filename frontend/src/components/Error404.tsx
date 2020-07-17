71da9e91e86843e3d0e286279dac32a6ab837262
adrienmntr <adrien.matthieu.monteiro@gmail.com>
2020-07-17 17:47:43 +0200

    Apply prettier

--- frontend/src/components/Error404.tsx
+++ frontend/src/components/Error404.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import MainContainer from "./MainContainer";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Error404() {
-    return (
-        <MainContainer heading={"404."}>
-            <p>
-                T’as essayé <Link to="/admin/">/admin/</Link> ?
-            </p>
-        </MainContainer>
-    );
+  return (
+    <MainContainer heading={"404."}>
+      <p>
+        T’as essayé <Link to="/admin/">/admin/</Link> ?
+      </p>
+    </MainContainer>
+  );
 }
