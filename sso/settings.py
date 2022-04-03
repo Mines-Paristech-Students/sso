4c51a0e1a36f23e9b4dba7af17f3bd2281a6acca
Louis Delmas <louis.delmas@outlook.com>
2022-04-03 23:40:17 +0200

    Update ALLOWED_HOSTS
    Update .env.dist

--- sso/settings.py
+++ sso/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ["localhost"] + [env.str("FRONTEND_HOST")] + ["127.0.0.1"]
 
 
 # Application definition
