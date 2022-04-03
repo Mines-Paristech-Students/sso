11edb24614a86f1f8627eb61dde8472fbffdc10b
Louis Delmas <louis.delmas@outlook.com>
2022-04-03 23:13:04 +0200

    Docker prod environment

--- sso/settings.py
+++ sso/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
